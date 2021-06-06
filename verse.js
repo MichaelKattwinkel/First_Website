838f683fb22f985d0ae7d07d19bce07a3480cee3
MichaelKattwinkel <mdkattwinkel@vt.edu>
2021-06-06 14:25:54 -0400

    Changing verse start and end formatting

--- verse.js
+++ verse.js
@@ -9,7 +9,8 @@ class Verse {
 
   getref() {
     let ref = "";
-    ref = this.book + " " + this.chapter + ":" + this.start + "-" + this.end;
+    ref = this.book + " " + this.chapter + ":" + this.start;
+    ref += this.start == this.end ? "" : "-" + this.end; // if they are equal dont include the dash
     return ref;
   }
 }
